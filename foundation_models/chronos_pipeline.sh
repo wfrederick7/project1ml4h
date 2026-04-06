effc13a7be67f2f353be1790cacea2f2c52dc43d
ivo-ab <ivoaban@gmail.com>
2026-04-06 13:46:18 +0200

    fixed hard-coded user paths

--- foundation_models/chronos_pipeline.sh
+++ foundation_models/chronos_pipeline.sh
@@ -6,12 +6,12 @@
 #SBATCH --output=chronos.out
 #SBATCH --error=chronos.err
 
-cd /home/wfrederick/project1ml4h/foundation_models
+cd /home/$USER/project1ml4h/foundation_models
 
 source /cluster/courses/ml4h/jupyter/bin/activate
 
-export PYTHONPATH=/home/wfrederick/project1ml4h/libs:$PYTHONPATH
-export HF_HOME=/home/wfrederick/project1ml4h/hf_cache
+export PYTHONPATH=/home/$USER/project1ml4h/libs:$PYTHONPATH
+export HF_HOME=/home/$USER/project1ml4h/hf_cache
 
 OLLAMA_MODELS=/cluster/courses/ml4h/llm/models OLLAMA_HOST=127.0.0.1:11435 /cluster/courses/ml4h/llm/bin/ollama serve &
 
