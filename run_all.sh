2097b4fb7b14f8b04965b420db0dd881633bb4be
ivo-ab <ivoaban@gmail.com>
2026-04-09 23:53:27 +0200

    fix run_all commented out parts

--- run_all.sh
+++ run_all.sh
@@ -16,26 +16,26 @@ if [ ! -d "$VENV_DIR" ]; then
 fi
 source ./activate_env.sh
 
-# echo "=== Data Preparation ==="
-# sbatch --wait data_prep/preprocess.sh
-# sbatch --wait data_prep/exploratory.sh
-# sbatch --wait data_prep/data_processing.sh
-# sbatch --wait data_prep/horn_data_processing.sh
+echo "=== Data Preparation ==="
+sbatch --wait data_prep/preprocess.sh
+sbatch --wait data_prep/exploratory.sh
+sbatch --wait data_prep/data_processing.sh
+sbatch --wait data_prep/horn_data_processing.sh
 
-# echo "=== Supervised Learning ==="
-# sbatch --wait supervised_learning/classic_ML_1.sh
-# sbatch --wait supervised_learning/classic_ML_2.sh
-# sbatch --wait supervised_learning/rnns.sh
-# sbatch --wait supervised_learning/simple_transformer.sh
-# sbatch --wait supervised_learning/horn_and_simple.sh
+echo "=== Supervised Learning ==="
+sbatch --wait supervised_learning/classic_ML_1.sh
+sbatch --wait supervised_learning/classic_ML_2.sh
+sbatch --wait supervised_learning/rnns.sh
+sbatch --wait supervised_learning/simple_transformer.sh
+sbatch --wait supervised_learning/horn_and_simple.sh
 
-# echo "=== Representation Learning ==="
-# sbatch --wait rep_learning/rep_learn.sh
+echo "=== Representation Learning ==="
+sbatch --wait rep_learning/rep_learn.sh
 
-# echo "=== Foundation Models ==="
-# sbatch --wait foundation_models/predict_evaluate_zero.sh
-# sbatch --wait foundation_models/predict_evaluate_few.sh
-# sbatch --wait foundation_models/predict_evaluate_emb.sh
+echo "=== Foundation Models ==="
+sbatch --wait foundation_models/predict_evaluate_zero.sh
+sbatch --wait foundation_models/predict_evaluate_few.sh
+sbatch --wait foundation_models/predict_evaluate_emb.sh
 sbatch --wait foundation_models/chronos_pipeline.sh
 sbatch --wait foundation_models/plotting.sh
 
